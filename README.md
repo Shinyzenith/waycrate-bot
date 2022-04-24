e3a222820af1b50ad6642d6cc3b364e0227bf934
Shinyzenith <aakashsensharma@gmail.com>
2022-04-24 19:18:30 +0530

    [push]

---
+++ README.md
@@ -0,0 +1,9 @@
+# Waycrate bot.
+
+Provide a token in .env.
+
+```bash
+git clone https://github.com/shinyzenith/waycrate/bot;cd waycrate-bot
+sudo python3 -m pip install -r ./requirements.txt
+python3.10 ./src/bot.py
+```
---
+++ src/bot.py
@@ -0,0 +1,38 @@
+import logging
+import os
+from pathlib import Path
+
+import coloredlogs
+import disnake
+from disnake.ext import commands
+from dotenv import load_dotenv
+
+
+class WaycrateBot(commands.Bot):
+    def __init__(self):
+        load_dotenv()
+        intents = disnake.Intents.default()
+        intents.members = True
+        # intents.message_content = True
+
+        super().__init__(reload=True, intents=intents) # Live reload of cogs on edits.
+
+        self.log = logging.getLogger("WaycrateBot")
+        coloredlogs.install(logger=self.log)
+        logging.basicConfig(level=logging.INFO, format="(%(asctime)s) %(levelname)s %(message)s", datefmt="%m/%d/%y - %H:%M:%S %Z")
+
+    def load_cogs(self):
+        for file in os.listdir(str(Path(__file__).parents[0])+"/cogs"):
+            if file.endswith(".py") and not file.startswith("_"):
+                self.load_extension(f"cogs.{file[:-3]}")
+
+        self.log.info("All cogs have been loaded. ")
+
+    def run(self):
+        if not os.getenv("BOT_TOKEN") or os.getenv("BOT_TOKEN") == "":
+            return self.log.error("No .env file setup with proper token paramter.")
+        self.load_cogs()
+        super().run(os.getenv("BOT_TOKEN"))
+
+bot = WaycrateBot()
+bot.run()
