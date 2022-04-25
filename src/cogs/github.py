f58723b467269692d29ce098b6df08c6d6632e81
Maks <max.centeno64@gmail.com>
2022-04-25 20:42:33 +1000

    fuck off pycache

--- src/cogs/github.py
+++ src/cogs/github.py
@@ -5,7 +5,6 @@ import aiohttp
 import coloredlogs
 import disnake
 from disnake.ext import commands, tasks
-from pydantic import Field
 
 test_guilds=[int(os.getenv("test_guild"))]
 
@@ -21,16 +20,12 @@ class Github(commands.Cog):
     async def on_ready(self):
         log.warn(f"{self.__class__.__name__} Cog has been loaded")
 
-    @tasks.loop(seconds=3000, reconnect=False)
+    @tasks.loop(seconds=30, reconnect=False)
     async def notify_task(self):
         await self.bot.wait_until_ready()
         res = await api_call(f"{os.getenv('API_BASE_URL')}repos/waycrate/swhkd")
         chan = self.bot.get_channel(int(os.getenv("STATS_CHANNEL")))
         chan.edit(name=f"Stars: {res['stargazers_count']} ⭐")
-
-            
-
-
 
 
     @commands.slash_command(guild_ids=test_guilds)
@@ -48,6 +43,19 @@ class Github(commands.Cog):
             await inter.response.send_message(f"{res['stargazers_count']}", ephemeral=True)
         elif field == "forks":
             await inter.response.send_message(f"{res['forks']}", ephemeral=True)
+            
+    @commands.slash_command(guild_ids=test_guilds)
+    @commands.cooldown(10, 60, commands.BucketType.guild)
+    @commands.guild_only()
+    async def get_info(self, inter: disnake.ApplicationCommandInteraction):
+        """Report a security vunerability.
+
+        Parameters
+        ----------
+        field: The type of information you're looking for.
+        """
+        res = await api_call(f"{os.getenv('API_BASE_URL')}repos/waycrate/swhkd")
+        await inter.response.send_message(f"{res['stargazers_count']}", ephemeral=True)
 
 async def api_call(call_url):
     async with aiohttp.ClientSession() as session:
