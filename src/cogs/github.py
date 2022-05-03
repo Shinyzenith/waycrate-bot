c3841e919b41323321086c7b3a42ba9ca2a75582
Maks <max.centeno64@gmail.com>
2022-05-03 16:42:00 +1000

    welcome msg + modal report

--- src/cogs/github.py
+++ src/cogs/github.py
@@ -5,6 +5,7 @@ import aiohttp
 import coloredlogs
 import disnake
 from disnake.ext import commands, tasks
+from disnake.enums import TextInputStyle
 
 test_guilds=[int(os.getenv("test_guild"))]
 
@@ -20,6 +21,8 @@ class Github(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         log.warn(f"{self.__class__.__name__} Cog has been loaded")
+        await self.bot.change_presence(status=disnake.Status.idle, activity=disnake.Game("Waycrate"))
+        
 
     @tasks.loop(seconds=1800, reconnect=False)
     async def stats_task(self):
@@ -29,10 +32,10 @@ class Github(commands.Cog):
         await chan.edit(name=f"Stars: {res['stargazers_count']} ⭐")
 
 
-    @commands.slash_command(guild_ids=test_guilds)
+    @commands.slash_command(guild_ids=test_guilds, description="Get stats about WayCrate")
     @commands.cooldown(10, 60, commands.BucketType.guild)
     @commands.guild_only()
-    async def get_info(self, inter: disnake.ApplicationCommandInteraction, field:Literal["stars", "forks"]):
+    async def info(self, inter: disnake.ApplicationCommandInteraction, field:Literal["stars", "forks"]):
         """Get information about waycrate tools.
 
         Parameters
@@ -45,54 +48,49 @@ class Github(commands.Cog):
         elif field == "forks":
             await inter.response.send_message(f"{res['forks']} forks")
             
-    @commands.slash_command(guild_ids=test_guilds)
+    @commands.slash_command(guild_ids=test_guilds, description="Report a security vulnerability.")
     @commands.cooldown(10, 60, commands.BucketType.guild)
     @commands.guild_only()
-    async def report(self, inter: disnake.ApplicationCommandInteraction):
-        """Report a security vunerability.
-
-        Parameters
-        ----------
-        field: The type of information you're looking for.
-        """
-        await inter.response.send_modal(modal=ReportModal())
-        
-        
-class ReportModal(disnake.ui.Modal):
-    def __init__(self) -> None:
-        components = [
+    async def security(self, inter: disnake.ApplicationCommandInteraction) -> None:
+        await inter.response.send_modal(
+            title="Report a Security Vunerability",
+            custom_id="report1",
+            components=[
                 disnake.ui.TextInput(
-                label="Email",
-                placeholder="Your email for futher communication",
-                custom_id="email",
-                style=disnake.TextInputStyle.short,
-                min_length=5,
-                max_length=30,
-            ),
-            disnake.ui.TextInput(
-                label="Description",
-                placeholder="The description of the bug",
-                custom_id="description",
-                style=disnake.TextInputStyle.paragraph,
-                min_length=20,
-                max_length=1024,
-            ),
-            disnake.ui.TextInput(
-                label="How to reproduce",
-                placeholder="How to reproduce the bug",
-                custom_id="content",
-                style=disnake.TextInputStyle.paragraph,
-                min_length=30,
-                max_length=1024,
-            ),
-        ]
-        super().__init__(title="Report a Security Vunerability", custom_id="create_tag", components=components)
+                    label="Email",
+                    placeholder="For Futher Contact",
+                    custom_id="email",
+                    style=TextInputStyle.short,
+                    max_length=50,
+                ),
+                disnake.ui.TextInput(
+                    label="Description Of The Bug",
+                    placeholder="What does the bug do?",
+                    custom_id="description",
+                    style=TextInputStyle.paragraph,
+                ),
+                   disnake.ui.TextInput(
+                    label="How To Reproduce",
+                    placeholder="How to reproduce the bug?",
+                    custom_id="reproduce",
+                    style=TextInputStyle.paragraph,
+                ),
+            ],
+        )
 
-    async def callback(self, inter: disnake.ModalInteraction) -> None:
-        embed = disnake.Embed(title="New Vunerability", color=0xFF0000)
-        for key, value in inter.text_values.items():
+        modal_inter: disnake.ModalInteraction = await self.bot.wait_for(
+            "modal_submit",
+            check=lambda i: i.custom_id == "report1" and i.author.id == inter.author.id,
+        )
+
+        embed = disnake.Embed(title="New Report", color=disnake.Colour.red())
+        channel = self.bot.get_channel(int(os.getenv("VUNERABLE_CHANNEL")))
+        for key, value in modal_inter.text_values.items():
             embed.add_field(name=key.capitalize(), value=value, inline=False)
-        await inter.response.send_message(embed=embed)
+        await modal_inter.response.send_message("Thanks for reporting!")
+        await channel.send(embed=embed)
+        
+
 
 
 
---
+++ src/cogs/welcome.py
@@ -0,0 +1,27 @@
+import logging
+import os
+import coloredlogs
+import disnake
+from disnake.ext import commands, tasks
+
+test_guilds=[int(os.getenv("test_guild"))]
+
+log = logging.getLogger("Welcome cog")
+coloredlogs.install(logger=log)
+
+            
+class Welcome(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        log.warn(f"{self.__class__.__name__} Cog has been loaded")
+        
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        channel = self.bot.get_channel(int(os.getenv("WELCOMECHANNEL")))
+        await channel.send(f"<:crateway:970227529413693511> Welcome {member.mention}")
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Welcome(bot))
