cb79fd57a8130b9843a2d6a185008ba1d38bbe46
Maks <max.centeno64@gmail.com>
2022-05-16 22:07:55 +1000

    fixed info and report cmd

--- src/cogs/github.py
+++ src/cogs/github.py
@@ -7,12 +7,11 @@ import disnake
 from disnake.ext import commands, tasks
 from disnake.enums import TextInputStyle
 
-test_guilds=[int(os.getenv("test_guild"))]
+test_guilds = [int(os.getenv("test_guild"))]
 
 log = logging.getLogger("Github cog")
 coloredlogs.install(logger=log)
 
-            
 class Github(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
@@ -22,7 +21,6 @@ class Github(commands.Cog):
     async def on_ready(self):
         log.warn(f"{self.__class__.__name__} Cog has been loaded")
         await self.bot.change_presence(status=disnake.Status.idle, activity=disnake.Game("Waycrate"))
-        
 
     @tasks.loop(seconds=1800, reconnect=False)
     async def stats_task(self):
@@ -32,10 +30,9 @@ class Github(commands.Cog):
         await chan.edit(name=f"Stars: {res['stargazers_count']} ⭐")
 
 
-    @commands.slash_command(guild_ids=test_guilds, description="Get stats about WayCrate")
-    @commands.cooldown(10, 60, commands.BucketType.guild)
+    @commands.slash_command(description="Get stats about WayCrate")
     @commands.guild_only()
-    async def info(self, inter: disnake.ApplicationCommandInteraction, field:Literal["stars", "forks"]):
+    async def info(self, inter: disnake.ApplicationCommandInteraction, field: Literal["stars", "forks", "total"]):
         """Get information about waycrate tools.
 
         Parameters
@@ -44,12 +41,22 @@ class Github(commands.Cog):
         """
         res = await api_call(f"{os.getenv('API_BASE_URL')}repos/waycrate/swhkd")
         if field == "stars":
-            await inter.response.send_message(f"{res['stargazers_count']} stars")
+            stars_embed = disnake.Embed(
+                title="Stars", description=f"{res['stargazers_count']} stars", color=disnake.Color.from_rgb(221, 161, 4))
+            await inter.response.send_message(embed=stars_embed)
         elif field == "forks":
-            await inter.response.send_message(f"{res['forks']} forks")
-            
-    @commands.slash_command(guild_ids=test_guilds, description="Report a security vulnerability.")
-    @commands.cooldown(10, 60, commands.BucketType.guild)
+            forks_embed = disnake.Embed(
+                title="Forks", description=f"{res['forks_count']} forks", color=disnake.Color.from_rgb(221, 161, 4))
+            await inter.response.send_message(embed=forks_embed)
+        elif field == "total":
+            res2 = await api_call(f"{os.getenv('API_BASE_URL')}orgs/waycrate/repos")
+            embed = disnake.Embed(title="Total Stats", color=disnake.Color.from_rgb(221, 161, 4))
+            embed.set_thumbnail(url="https://waycrate.github.io/assets/img/waycrate-logo.png")
+            for x in res2:
+             embed.add_field(name=x["name"], value=f"{x['stargazers_count']} stars")
+            await inter.response.send_message(embed=embed)
+
+    @commands.slash_command(description="Report a security vulnerability.")
     @commands.guild_only()
     async def security(self, inter: disnake.ApplicationCommandInteraction) -> None:
         await inter.response.send_modal(
@@ -69,7 +76,7 @@ class Github(commands.Cog):
                     custom_id="description",
                     style=TextInputStyle.paragraph,
                 ),
-                   disnake.ui.TextInput(
+                disnake.ui.TextInput(
                     label="How To Reproduce",
                     placeholder="How to reproduce the bug?",
                     custom_id="reproduce",
@@ -87,18 +94,14 @@ class Github(commands.Cog):
         channel = self.bot.get_channel(int(os.getenv("VUNERABLE_CHANNEL")))
         for key, value in modal_inter.text_values.items():
             embed.add_field(name=key.capitalize(), value=value, inline=False)
-        await modal_inter.response.send_message("Thanks for reporting!")
-        await channel.send(embed=embed)
-        
-
-
-
+        await modal_inter.response.send_message("Thanks for reporting!", empheral=True)
+        await channel.send(embed=embed)  
 
 async def api_call(call_url):
     async with aiohttp.ClientSession() as session:
-            async with session.get(call_url) as response:
-                response = await response.json()
-                return response
+        async with session.get(call_url) as response:
+            response = await response.json()
+            return response
 
 
 def setup(bot: commands.Bot):
